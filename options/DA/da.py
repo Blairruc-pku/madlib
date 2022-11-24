0c2b7f1f5022474e2cc262ad8f953a9bfe3a8c26
Blairruc-pku <zhang_xinyi@pku.edu.cn>
2022-11-24 09:43:43 +0800

    modify GA

--- options/DA/da.py
+++ options/DA/da.py
@@ -1,7 +1,6 @@
 import os
 import pdb
 import sys
-import pandas as pd
 import numpy as np
 import psycopg2 as p2
 import tensorflow as tf
@@ -9,8 +8,8 @@ import datetime
 import time
 sys.path.append('..')
 from OUTDB.DeepFM import DeepFM
-from grad_avg import config
-from grad_avg.metrics import accuracy
+from OUTDB import config
+from OUTDB.metrics import accuracy
 
 logfile = os.path.join('../logs', 'da_' + str(int(time.time())) + '.res')
 
@@ -21,7 +20,6 @@ def log_record(content, ifprint = True):
         f.write(content_out + '\n')
         if ifprint:
             print(content_out)
-
 
 
 
@@ -50,9 +48,6 @@ class DeepFM_DA(DeepFM):
         log_record("Initialized model")
 
 
-
-
-
     def _connect_db(self):
         conn = p2.connect(host=self.host, user=self.user, dbname=self.dbname, port=self.port)
         return conn
@@ -72,6 +67,7 @@ class DeepFM_DA(DeepFM):
             return results
         except:
             return  None
+
 
     def get_batch_da(self, batch_size, index):
         time_begin = time.time()
@@ -171,35 +167,8 @@ class DeepFM_DA(DeepFM):
             log_record("Total on epoch {} takes {} sec".format(epoch, time.time() - time_begin_epoch))
 
 
-
-def _load_data():
-
-    dfTrain = pd.read_csv(config.TRAIN_FILE)
-    dfTest = pd.read_csv(config.TEST_FILE)
-
-    def preprocess(df):
-        cols = [c for c in df.columns if c not in ["id", "target"]]
-        df["missing_feat"] = np.sum((df[cols] == -1).values, axis=1)
-        df["ps_car_13_x_ps_reg_03"] = df["ps_car_13"] * df["ps_reg_03"]
-        return df
-
-    dfTrain = preprocess(dfTrain)
-    dfTest = preprocess(dfTest)
-
-    cols = [c for c in dfTrain.columns if c not in ["id", "target"]]
-    cols = [c for c in cols if (not c in config.IGNORE_COLS)]
-
-    X_train = dfTrain[cols].values
-    y_train = dfTrain["target"].values
-    X_test = dfTest[cols].values
-    ids_test = dfTest["id"].values
-    cat_features_indices = [i for i,c in enumerate(cols) if c in config.CATEGORICAL_COLS]
-
-    return dfTrain, dfTest, X_train, y_train, X_test, ids_test, cat_features_indices
-
-
-
-dfm_params = {
+if __name__ == "__main__":
+    dfm_params = {
     "use_fm": True,
     "use_deep": True,
     "embedding_size": 8,
@@ -217,42 +186,13 @@ dfm_params = {
     "verbose": True,
     "eval_metric": accuracy,
     "random_seed": config.RANDOM_SEED
-}
+    }
+
+    log_record("Begin DA")
+    model_global = DeepFM_DA(**dfm_params)
+    model_global.fit()
 
 
-# # load data
-# dfTrain, dfTest, X_train, y_train, X_test, ids_test, cat_features_indices = _load_data()
-#
-#
-# # folds
-# folds = list(StratifiedKFold(n_splits=config.NUM_SPLITS, shuffle=True,
-#                              random_state=config.RANDOM_SEED).split(X_train, y_train))
-#
-# fd = FeatureDictionary(dfTrain=dfTrain, dfTest=dfTest,
-#                        numeric_cols=config.NUMERIC_COLS,
-#                        ignore_cols=config.IGNORE_COLS)
-# data_parser = DataParser(feat_dict=fd)
-# Xi_train, Xv_train, y_train = data_parser.parse(df=dfTrain, has_label=True)
-
-# data_all = np.hstack((np.array(Xi_train),np.array(Xv_train),np.array(y_train).reshape(-1,1)))
-# df_all = pd.DataFrame(data_all)
-# df_all.to_csv('data/driving_tf.csv',header=0)
-# Xi_test, Xv_test, ids_test = data_parser.parse(df=dfTest)
-
-# dfm_params["feature_size"] = fd.feat_dim
-# dfm_params["field_size"] = len(Xi_train[0])
-# print(dfm_params["feature_size"])
-
-log_record("Begin DA")
-model_global = DeepFM_DA(**dfm_params)
-model_global.fit()
-
-# _get = lambda x, l: [x[i] for i in l]
-# for i, (train_idx, valid_idx) in enumerate(folds):
-#     Xi_train_, Xv_train_, y_train_ = _get(Xi_train, train_idx), _get(Xv_train, train_idx), _get(y_train, train_idx)
-#     Xi_valid_, Xv_valid_, y_valid_ = _get(Xi_train, valid_idx), _get(Xv_train, valid_idx), _get(y_train, valid_idx)
-#     model_global.fit(Xi_train_, Xv_train_, y_train_)
-#     pdb.set_trace()
 
 
 
