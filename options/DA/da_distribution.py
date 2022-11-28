946c4773564c914baaf557af0c6dcd15c4893d75
Blairruc-pku <zhang_xinyi@pku.edu.cn>
2022-11-28 11:20:39 +0800

    update DA distribution

--- options/DA/da_distribution.py
+++ options/DA/da_distribution.py
@@ -3,9 +3,11 @@ import pdb
 import sys
 import numpy as np
 import psycopg2 as p2
+import threading
 import tensorflow as tf
 import datetime
 import time
+from collections import  OrderedDict
 from utils import serialize_nd_weights, deserialize_as_nd_weights, deserialize_gradient, serialize_gradient
 from tensorflow.python.framework.ops import IndexedSlicesValue
 sys.path.append('..')
@@ -23,155 +25,6 @@ def log_record(content, ifprint = True):
         if ifprint:
             print(content_out)
 
-class DeepFM_DA(DeepFM):
-    def __init__(self, **kwargs):
-        self.user = 'ruike.xy'
-        self.host = 'localhost'
-        self.dbname = 'driving'
-        self.sample_tbl = 'driving'
-        self.port = 5432
-        self.total_sample = self._fetch_results("select count(*) from {self.sample_tbl}".format(**locals()))[0][0]
-        self.Xi_train, self.Xv_train, self.y_train = list(), list(), list()
-        columns = self._fetch_results("select column_name FROM information_schema.columns WHERE table_name  ='{self.sample_tbl}'".format(**locals()))
-        self.xi_index = len(columns) / 2
-        index_columns = columns[0:self.xi_index-1]
-        feat_dim = 0
-        for column in index_columns:
-            column = column[0]
-            num = self._fetch_results("select count(distinct {column}) from {self.sample_tbl}".format(**locals()))[0][0]
-            feat_dim = feat_dim + num
-        kwargs["field_size"] = self.xi_index - 1
-        kwargs["feature_size"] = feat_dim
-        log_record("Feat_dim:{}".format(feat_dim))
-
-        DeepFM.__init__(self, **kwargs)
-        log_record("Initialized model")
-
-
-    def _connect_db(self):
-        conn = p2.connect(host=self.host, user=self.user, dbname=self.dbname, port=self.port)
-        return conn
-
-    def _execute(self, sql):
-        conn = self._connect_db()
-        cursor = conn.cursor()
-        cursor.execute(sql)
-        if cursor: cursor.close()
-        if conn: conn.close()
-
-    def _fetch_results(self, sql, json=False):
-        conn = self._connect_db()
-        cursor = conn.cursor()
-        try:
-            cursor.execute(sql)
-            results = cursor.fetchall()
-            if cursor: cursor.close()
-            if conn: conn.close()
-
-            if json:
-                columns = [col[0] for col in cursor.description]
-                return [dict(zip(columns, row)) for row in results]
-            return results
-        except:
-            return  None
-
-
-    def get_batch_da(self, batch_size, index):
-        time_begin = time.time()
-        start = index * batch_size
-        end = (index+1) * batch_size
-        end = end if end < self.total_sample else self.total_sample
-        select_query = "select * from {self.sample_tbl} where id >= {start} and id< {end}".format(**locals())
-        data = self._fetch_results(select_query)
-        xi = [item[1:self.xi_index] for item in data]
-        xv = [item[self.xi_index: -1] for item in data]
-        y = [[item[-1]] for item in data]
-        self.Xi_train = self.Xi_train + xi
-        self.Xv_train = self.Xv_train + xv
-        self.y_train = self.y_train + y
-        log_record("get batch {} takes {} sec".format(index, time.time() - time_begin), False)
-
-        return xi, xv, y
-
-
-    def predict(self, Xi=None, Xv=None):
-        """
-        :param Xi: list of list of feature indices of each sample in the dataset
-        :param Xv: list of list of feature values of each sample in the dataset
-        :return: predicted probability of each sample
-        """
-        if not Xi is None:
-            return DeepFM.predict(self, Xi, Xv)
-
-        batch_index = 0
-        Xi_batch, Xv_batch, y_batch = self.get_batch_da( self.batch_size, batch_index)
-        y_pred = None
-        while len(Xi_batch) > 0:
-            time_begin_infer = time.time()
-            num_batch = len(y_batch)
-            feed_dict = {self.feat_index: Xi_batch,
-                         self.feat_value: Xv_batch,
-                         self.label: y_batch,
-                         self.dropout_keep_fm: [1.0] * len(self.dropout_fm),
-                         self.dropout_keep_deep: [1.0] * len(self.dropout_deep),
-                         self.train_phase: False}
-            batch_out = self.sess.run(self.out, feed_dict=feed_dict)
-            log_record("Infer on batch {} takes {} sec".format(batch_index, time.time() - time_begin_infer), False)
-
-            if batch_index == 0:
-                y_pred = np.reshape(batch_out, (num_batch,))
-            else:
-                y_pred = np.concatenate((y_pred, np.reshape(batch_out, (num_batch,))))
-
-            batch_index += 1
-            Xi_batch, Xv_batch, y_batch = self.get_batch_da( self.batch_size, batch_index)
-
-        return y_pred
-
-
-    def evaluate(self, Xi, Xv, y):
-        y_pred = self.predict(Xi, Xv)
-        y = [[float(item[0])] for item in y]
-        return self.eval_metric(y, y_pred)
-
-
-    def fit(self, Xi_valid=None, Xv_valid=None, y_valid=None,
-            early_stopping=False, refit=False):
-
-        has_valid = Xv_valid is not None
-        for epoch in range(self.epoch):
-            t1 = time.time()
-            total_batch = int(self.total_sample / self.batch_size)
-            log_record("Total batch:{}".format(total_batch))
-            time_begin_epoch = time.time()
-            for i in range(total_batch):
-                Xi_batch, Xv_batch, y_batch = self.get_batch_da( self.batch_size, i)
-                time_begin_batch = time.time()
-                self.fit_on_batch(Xi_batch, Xv_batch, y_batch)
-                if i % 50 == 0:
-                    log_record("Fit on batch {} takes {} sec".format(i, time.time() - time_begin_batch))
-                else:
-                    log_record("Fit on batch {} takes {} sec".format(i, time.time() - time_begin_batch), False)
-            log_record("Train on epoch {} takes {} sec".format(epoch, time.time() - time_begin_epoch))
-            # evaluate training and validation datasets
-            time_begin_evaluate = time.time()
-            train_result = self.evaluate(self.Xi_train, self.Xv_train, self.y_train)
-            self.train_result.append(train_result)
-            log_record("Evaluate on epoch {} takes {} sec".format(epoch, time.time() - time_begin_evaluate))
-            if has_valid:
-                valid_result = self.evaluate(Xi_valid, Xv_valid, y_valid)
-                self.valid_result.append(valid_result)
-            if self.verbose > 0 and epoch % self.verbose == 0:
-                if has_valid:
-                    print("[%d] train-result=%.4f, valid-result=%.4f [%.1f s]"
-                          % (epoch + 1, train_result, valid_result, time.time() - t1))
-                else:
-                    print("[%d] train-result=%.4f [%.1f s]"
-                          % (epoch + 1, train_result, time.time() - t1))
-            if has_valid and early_stopping and self.training_termination(self.valid_result):
-                break
-
-            log_record("Total on epoch {} takes {} sec".format(epoch, time.time() - time_begin_epoch))
 
 class Schema:
     Model_Table = 'model'
@@ -180,7 +33,7 @@ class Schema:
 class DeepFM_DA(DeepFM):
     def __init__(self, **kwargs):
         self.user = 'ruike.xy'
-        self.host = 'localhost'
+        self.host = '11.164.101.172'
         self.dbname = 'driving'
         self.sample_tbl = 'driving'
         self.port = 5432
@@ -234,9 +87,19 @@ class DeepFM_DA(DeepFM):
         sql_check_exists = "SELECT EXISTS( SELECT 1 FROM pg_class, pg_namespace WHERE relnamespace = pg_namespace.oid AND relname='{name}') as table_exists;".format(**locals())
         return self._fetch_results(sql_check_exists)[0][0]
 
+    def clear(self):
+        sql = "select pid, query from pg_stat_activity where datname='{self.sample_tbl}';".format(**locals())
+        results = self._fetch_results(sql)
+        for row in results:
+            pid, query = row
+            if not 'pg_stat_activity' in query:
+                self._execute("select pg_terminate_backend({pid})".format(**locals()))
+
+
 class DeepFM_Master( DeepFM_DA):
     def __init__(self, **kwargs):
         DeepFM_DA.__init__(self, **kwargs)
+        self.clear()
         self.register_model()
         self.version = dict()
 
@@ -304,45 +167,49 @@ class DeepFM_Master( DeepFM_DA):
             sess.run(update)
             weights_updated = [sess.run(v) for v in variables_]
 
-
         self.save_weight(weights_updated)
-
         return weights_updated
 
-    def apply_grads_loop(self, worker_id):
+    def apply_grads_loop(self):
         while True:
-            self.apply_grads(worker_id)
+            self.apply_grads()
 
-    def apply_grads(self, worker_id):
+    def apply_grads(self):
         gradient_table = Schema.GRADIENT_TABLE
-        if not worker_id in self.version.keys():
-            self.version[worker_id] = 0
+        query = '''SELECT worker_id, version FROM {gradient_table} WHERE model_id={self.model_id}'''.format(**locals())
+        results = self._fetch_results(query)
+        for row in results:
+            worker_id, version = row
+            if not worker_id in self.version.keys():
+                self.version[worker_id] = 0
+            if version == self.version[worker_id]:
+                self.apply_grads_per_worker(worker_id)
+
+
+    def apply_grads_per_worker(self, worker_id):
+        gradient_table = Schema.GRADIENT_TABLE
         version = self.version[worker_id]
         query = '''SELECT gradient, shape, auxiliaries FROM {gradient_table} WHERE model_id={self.model_id} AND version={version} AND worker_id={worker_id}'''.format(**locals())
         results = self._fetch_results(query)
-        if results == []:
-            #log_record("No gradient with version {version}".format(**locals()))
-            return
-
-        log_record("Master receive gradients from worker {} with version {}".format(worker_id, version))
+        log_record("[Master] [Worker{}] Receive gradients with version {}".format(worker_id, version))
         gradient_serialized, shape, auxiliaries = results[0]
         gradients = deserialize_gradient(gradient_serialized, eval(shape), eval(auxiliaries))
         self.update(gradients)
         self.version[worker_id] = self.version[worker_id] + 1
 
-        query = "UPDATE {} SET model_version = {}".format(gradient_table, self.version[worker_id])
+        query = "UPDATE {} SET model_version={} WHERE model_id={} AND worker_id={}".format(gradient_table, self.version[worker_id], self.model_id,worker_id)
         self._execute(query)
-        log_record("Master save model with version {} for worker {}".format(self.version[worker_id], worker_id))
+        log_record("[Master] [Worker{}] Save model with version {}".format(worker_id, self.version[worker_id]))
 
 
 class DeepFM_Worker(DeepFM_DA):
-    def __init__(self, **kwargs):
+    def __init__(self, worker_id, **kwargs):
         DeepFM_DA.__init__(self, **kwargs)
-        total_batch = int(self.total_sample / self.batch_size)
-        self.worker_id = 0
+        self.worker_id = worker_id
         self.model_id = self._fetch_results("SELECT max(model_id) from model".format(**locals()))[0][0]
         self.version = 0
-
+        self.total_sample_worker = self._fetch_results("SELECT count(*) FROM {self.sample_tbl} WHERE gp_segment_id={self.worker_id}".format(**locals()))[0][0]
+        self.get_block_info()
         variables = self.graph.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
         self.update_placehoders = list()
         self.update_ops = list()
@@ -355,19 +222,23 @@ class DeepFM_Worker(DeepFM_DA):
             grads_and_vars = self.optimizer.compute_gradients(self.loss)
             self.grad_op = [x[0] for x in grads_and_vars]
 
+        self.init_weight = True
+
     def log_variables(self, i):
         variable = self.graph.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)[i]
         variables_value = self.sess.run(variable)
         log_record(variables_value)
 
     def pull_weights(self):
-        result = self._fetch_results("SELECT model_version FROM {} WHERE worker_id={} AND model_id={} ".format(Schema.GRADIENT_TABLE, self.worker_id,self.model_id))
-        while not ((result==[] and self.version==0) or (result[0][0]==self.version)):
-            time.sleep(1)
-            log_record("Wait for model with {}".format(self.version))
-            result = self._fetch_results("SELECT model_version FROM {} WHERE worker_id={} AND model_id={} ".format(Schema.GRADIENT_TABLE, self.worker_id,self.model_id))
+        sql_check_version = "SELECT model_version FROM {} WHERE worker_id={} AND model_id={} ".format(Schema.GRADIENT_TABLE, self.worker_id,self.model_id)
+        result = self._fetch_results(sql_check_version)
+        while not (self.init_weight or (result[0][0]==self.version)):
+             result = self._fetch_results(sql_check_version)
+        if not self.init_weight:
+            log_record("[Worker{}] Pull weight with version {}".format(self.worker_id, self.version))
 
-
+        if self.init_weight:
+            self.init_weight = False
         result = self._fetch_results("SELECT weight, shape FROM model WHERE model_id =1")
         shapes_fetch = eval(result[0][1])
         weights = deserialize_as_nd_weights(result[0][0], shapes_fetch)
@@ -377,23 +248,57 @@ class DeepFM_Worker(DeepFM_DA):
         self.sess.run(self.update_ops, feed_dict=feed_dict)
 
 
+    def get_block_info(self):
+        query = "SELECT (ctid::text::point)[0]::bigint AS block_number, count(*) FROM {self.sample_tbl} where gp_segment_id={self.worker_id} group by block_number;".format(**locals())
+        results = self._fetch_results(query)
+        block_info = dict()
+        for row in results:
+            block_id, count = row
+            block_info[int(block_id)] = int(count)
 
-    def get_batch_da(self, batch_size, index):
+        self.block_info = OrderedDict(sorted(block_info.items()))
+
+
+    def check_ctid(self, ctid):
+        fid, sid = ctid
+        count = 0
+        for block_id in self.block_info.keys():
+            if block_id < fid:
+                count = count + self.block_info[block_id]
+            else:
+                count = count + sid - 1
+                return count
+
+    def get_ctid(self, record_id):
+        record_cum = 0
+        for block_id in list(self.block_info.keys()):
+            if record_cum + self.block_info[block_id] >= record_id + 1:
+                res = (block_id, record_id - record_cum + 1)
+                assert  self.check_ctid(res) == record_id, "Wrong ctid generated"
+                return res
+            record_cum = record_cum + self.block_info[block_id]
+
+
+
+    def get_batch_data_block(self, batch_size, index):
         time_begin = time.time()
         start = index * batch_size
         end = (index+1) * batch_size
         end = end if end < self.total_sample else self.total_sample
-        select_query = "select * from {self.sample_tbl} where id >= {start} and id< {end}".format(**locals())
+        start_block = self.get_ctid(start)
+        end_block = self.get_ctid(end)
+        select_query = "select * from {self.sample_tbl} where ctid >= '{start_block}' and ctid< '{end_block}' AND gp_segment_id={self.worker_id}".format(**locals())
         data = self._fetch_results(select_query)
         xi = [item[1:self.xi_index] for item in data]
         xv = [item[self.xi_index: -1] for item in data]
         y = [[item[-1]] for item in data]
+        assert len(y) == end - start, "Number of data selected ({}) doesn't match requirement ({})).".format(len(y), end-start)
         self.Xi_train = self.Xi_train + xi
         self.Xv_train = self.Xv_train + xv
         self.y_train = self.y_train + y
-        log_record("get batch {} takes {} sec".format(index, time.time() - time_begin), False)
-
+        log_record("[Worker{}] Get batch {} takes {} sec".format(self.worker_id, index, time.time() - time_begin), False)
         return xi, xv, y
+
 
     def gradients_compute(self, xi, xv, y):
         feed_dict = {self.feat_index: xi,
@@ -406,6 +311,7 @@ class DeepFM_Worker(DeepFM_DA):
         loss, grads = self.sess.run([self.loss, self.grad_op], feed_dict=feed_dict)
 
         return grads
+
 
     def push_graident(self, grads):
         gradient_table = Schema.GRADIENT_TABLE
@@ -426,19 +332,20 @@ class DeepFM_Worker(DeepFM_DA):
             cursor.execute(sql_update, (p2.Binary(grads_serialized), str(shapes), str(auxiliaries)))
             conn.commit()
 
-        log_record("Worker {self.worker_id} push gradient with version {self.version}".format(**locals()))
+        log_record("[Worker{self.worker_id}] Push gradient with version {self.version}".format(**locals()))
         self.version = self.version + 1
 
     def run_one_batch(self, batch_id):
         self.pull_weights()
-        Xi_batch, Xv_batch, y_batch = self.get_batch_da( self.batch_size, batch_id)
+        Xi_batch, Xv_batch, y_batch = self.get_batch_data_block( self.batch_size, batch_id)
         grads = self.gradients_compute( Xi_batch, Xv_batch, y_batch)
         self.push_graident(grads)
 
     def run(self):
-        total_batch = int(self.total_sample / self.batch_size)
+        total_batch = int(self.total_sample_worker / self.batch_size)
         log_record("Total batch:{}".format(total_batch))
         for epoch in range(self.epoch):
+            log_record("[Worker{self.worker_id}] Enter epoch {epoch}".format(**locals()))
             for i in range(total_batch):
                 self.run_one_batch(i)
 
@@ -470,10 +377,16 @@ if __name__ == "__main__":
 
     log_record("Begin DA")
     model_master = DeepFM_Master(**dfm_params)
-    model_worker = DeepFM_Worker(**dfm_params)
-    import threading
-    m1 = threading.Thread(target=model_master.apply_grads_loop,  args=(0,), name='master').start()
-    model_worker.run()
+
+    master = threading.Thread(target=model_master.apply_grads_loop, name='master').start()
+    worker_num = 4
+
+    def setup_worker(worker_id, dfm_params):
+        model_worker = DeepFM_Worker(worker_id=worker_id, **dfm_params)
+        model_worker.run()
+
+    for i in range(worker_num):
+        threading.Thread(target=setup_worker, args=(i, dfm_params), name='worker{}'.format(i)).start()
 
 
 
